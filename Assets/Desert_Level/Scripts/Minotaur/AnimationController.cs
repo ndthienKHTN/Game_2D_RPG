6aa5cac2a7b6485d04c4ce11442e97a1b6cb4ef7
Phạm Đình Tiến <136001495+TienPham569us@users.noreply.github.com>
2024-12-02 21:32:17 +0700

    dev: add desert map

---
+++ Assets/Desert_Level/Scripts/Minotaur/AnimationController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
