351f72ea76ccae70ab5282ff4ec500ed2e4aedaf
binzj195 <huuphuoc6ccc@gmail.com>
2024-12-02 21:36:11 +0700

    Forest Level

---
+++ Assets/Forest_Level/Scripts/Collectable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
+}
