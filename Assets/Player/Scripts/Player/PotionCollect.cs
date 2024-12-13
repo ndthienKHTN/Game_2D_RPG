3a530c03ced559232ca9876175b06ed169d09e9c
Nguyen Duy Long <longnhos11@gmail.com>
2024-12-13 11:49:34 +0700

    Sword Damage + object Health + health bar+ arrow

---
+++ Assets/Player/Scripts/Player/PotionCollect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionCollect : MonoBehaviour
+{
+    //Detect collision trigger with player
+    private void  OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            if (player.health < player.maxHealth)
+            {
+                player.ChangeHealth(40);
+                Destroy(gameObject);
+            }
+        }
+        // Debug.Log("Collision with player");
+    }
+}
