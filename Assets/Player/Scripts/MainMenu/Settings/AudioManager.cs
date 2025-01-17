da686d5c84d37d26566f9a6e6eaf41678fe1220b
Wut_đz <118157095+WutScor@users.noreply.github.com>
2025-01-17 17:35:36 +0700

    Chỉnh map và chỉ số enemy

--- Assets/Player/Scripts/MainMenu/Settings/AudioManager.cs
+++ Assets/Player/Scripts/MainMenu/Settings/AudioManager.cs
@@ -15,7 +15,7 @@ public class AudioManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
